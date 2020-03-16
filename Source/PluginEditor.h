70789ba96dd3b57e46de70291e7b301fdb8dab14
Jacob Critch <jacobc@mun.ca>
2020-03-16 01:25:00 -0230

    Initial commit

---
+++ Source/PluginEditor.h
@@ -0,0 +1,39 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class PluginTestAudioProcessorEditor  : public AudioProcessorEditor, private Slider::Listener
+{
+public:
+    PluginTestAudioProcessorEditor (PluginTestAudioProcessor&);
+    ~PluginTestAudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    void sliderValueChanged (Slider* slider) override;
+    
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    PluginTestAudioProcessor& processor;
+    
+    Slider midiVolume;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginTestAudioProcessorEditor)
+};
